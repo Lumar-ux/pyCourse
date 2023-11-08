9d2ec4ada457df3a74ec2ba11dad90cb8e1a8b8c
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-11-08 11:50:06 +0100

    Update V4.py

--- V4.py
+++ V4.py
@@ -3,11 +3,13 @@ print("Bienvenue dans le jeu du motus")
 print("Devinez le mot en 5 tentatives\n")
 
 vie = 5
-actuel_fix = ["_"] * len(us)  # Initialise la liste des lettres fixes avec des underscores
+actuel_fix = ["_"] * len(us)  # On initialise le tableau des lettres fixes avec des underscores
 
 while vie > 0:
     print("Tentative restantes: " + str(vie))
-    print("Mot actuel : " + "".join(actuel_fix))  # Affiche le mot avec les lettres trouvées jusqu'à présent
+    # On affiche le mot actuel avec des espaces entre les lettres pour une meilleure lisibilité
+    print("Mot actuel : " + " ".join(actuel_fix))
+
     motin = input("Entrez un mot de 5 lettres : ")
 
     if len(motin) != 5:
@@ -18,15 +20,17 @@ while vie > 0:
         print("Bonne proposition, vous avez trouvé le motus!")
         break
 
-    actuel_tmp = ""
+    actuel_tmp = []  # On utilise une liste pour construire le mot temporaire
     for i in range(5):
         if motin[i] == us[i]:
-            actuel_tmp += "[" + motin[i] + "]"
-            actuel_fix[i] = motin[i]  # Mise à jour de la liste des lettres fixes
+            actuel_tmp.append(motin[i])  # On ajoute la lettre trouvée
+            actuel_fix[i] = motin[i]  # Mise à jour des lettres fixes
         else:
-            actuel_tmp += "[" + actuel_fix[i] + "]"  # Utilise les lettres fixes déjà découvertes
+            actuel_tmp.append("_")  # On ajoute un underscore pour une lettre non trouvée
 
-    print("Mauvaise proposition, mot actuel : " + actuel_tmp + "\n")
+    # On affiche le mot temporaire avec des crochets pour indiquer les lettres non trouvées
+    print("Mauvaise proposition, mot actuel : " + " ".join(["[" + char + "]" if char != "_" else "[_]" for char in actuel_tmp]))
+    print("\n")
     vie -= 1
 
     if vie == 0:
